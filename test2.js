e82a73c4b57b061d215480db2ff9aca688b08677
Your Name <shubhambabai8@gmail.com>
2022-07-31 10:55:54 +0530

    used match() function with global, ignorecase flag

--- test2.js
+++ test2.js
@@ -1,4 +1,10 @@
 let st = "This is my life.";
 let regex = /is/;
 let res = regex.test(st);
-console.log(res);
+// console.log(res);
+
+
+let atr_st = "This is Not my not problem";
+let atr_regex = /Not/ig;
+let atr_res = atr_st.match(atr_regex);
+console.log(atr_res);
