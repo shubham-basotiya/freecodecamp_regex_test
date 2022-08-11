4a71d1fbab96aae24ff527580183671bd7ba8c73
Your Name <shubhambabai8@gmail.com>
2022-08-11 18:03:23 +0530

    add information about $ sign

--- test2.js
+++ test2.js
@@ -14,7 +14,27 @@
 // let res = atr_str.match(atr_regex);
 // console.log(res);
 
-let atr_str = "This is another example of regex 01.";
-let atr_regex = /[a-z0-9]/ig;
-let res = atr_str.match(atr_regex);
-console.log(res);
+// let atr_str = "This is another example of regex 01.";
+// let atr_regex = /[a-z0-9]/ig;
+// let res = atr_str.match(atr_regex);
+// console.log(res);
+
+// let difficultSpelling = "Mississippi";
+// let myRegex = /s+/g; // Change this line
+// let result = difficultSpelling.match(myRegex);
+// console.log(result);
+
+// let difficultSpelling = "titanic";
+// let myRegex = /t[a-z]*?i/; // Change this line
+// let result = difficultSpelling.match(myRegex);
+
+// let text = "<h1>Winter is coming</h1>";
+// let myRegex = /<.*?>/; // Change this line
+// let result = text.match(myRegex);
+// console.log(result);
+
+let firstString = "Ricky is first and can be found.";
+let firstRegex = /^Ricky/;
+console.log(firstRegex.test(firstString));
+let notFirst = "You can't find Ricky now.";
+console.log(firstRegex.test(notFirst));
